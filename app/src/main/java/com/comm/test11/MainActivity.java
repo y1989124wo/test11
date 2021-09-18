f9497e2c7929e526d2a34626f8bb558a737e7435
yangj <445208195@qq.com>
2021-09-18 17:48:58 +0800

    测试撤回2

--- app/src/main/java/com/comm/test11/MainActivity.java
+++ app/src/main/java/com/comm/test11/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        temp = 2;
+        temp = 26;
     }
 }
