08db871d469f87f76193901896b24df3db74ca83
yangj <445208195@qq.com>
2021-09-18 17:20:56 +0800

    测试撤回

--- app/src/main/java/com/comm/test11/MainActivity.java
+++ app/src/main/java/com/comm/test11/MainActivity.java
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+   int temp = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      
+        temp = 2;
     }
 }
